6ca5f86bcc4cab68b248a2112dc336ef23e6630a
vaishnavi2038 <int.conquer2038@gmail.com>
2021-05-17 16:14:30 +0530

    resolved issues

--- prediction_website/settings.py
+++ prediction_website/settings.py
@@ -144,4 +144,4 @@ import dj_database_url
 prod_db  =  dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
 
-#ALLOWED_HOSTS = ['disease-prediction01.herokuapp.com']
+ALLOWED_HOSTS = ['disease-prediction-01.herokuapp.com']
